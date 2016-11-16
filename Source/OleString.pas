2aa73cb988ed63a151bf270a9c8810236729ba88
evgeny-k <git@evgenysoft.com>
2016-11-16 15:39:27 +0300

    76776: Island RTL: OleString

---
+++ Source/OleString.pas
@@ -0,0 +1,68 @@
+﻿namespace RemObjects.Elements.System;
+type
+  OleString = public class(Object)
+  private
+    bstr: ^Char;
+  protected
+    method AllocString(Value: String): ^Char;
+    begin
+      if assigned(Value) then 
+        exit rtl.SysAllocStringLen(Value.FirstChar, Value.Length)
+      else 
+        exit nil;
+    end;
+
+    method FreeString(Value : ^Char);
+    begin
+      if Value <> nil then rtl.SysFreeString(Value);
+    end;
+  public
+    constructor(Value: String);
+    begin
+      bstr := AllocString(Value);
+    end;
+
+    finalizer;
+    begin
+      FreeString(bstr);
+    end;
+
+    method ToString: String; override;
+    begin
+      if bstr = nil then 
+        exit nil
+      else 
+        exit String.FromPChar(bstr,rtl.SysStringLen(bstr));
+    end;
+
+    method GetHashCode: Integer; override;
+    begin
+      exit ToString().GetHashCode;
+    end;
+
+    method &Equals(Value: OleString): Boolean;
+    begin
+      exit ToString = Value.ToString;
+    end;
+
+    class operator Implicit(Value: String): OleString;
+    begin
+      if assigned(Value) then
+        exit new OleString(Value)
+      else
+        exit nil;
+    end;
+  end;
+
+  String_OleStringSupport = public extension class(String)
+  public
+    class operator Implicit(Value: OleString): String;
+    begin
+      if assigned(Value) then
+        exit Value.ToString
+      else 
+        exit nil;
+    end;
+  end;
+
+end.
