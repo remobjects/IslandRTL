732711f35bc6d8bdc808469823a311bc187bb16d
Carlo Kok <ck@remobjects.com>
2017-03-09 10:27:50 +0100

    0: assert is reverse, it shouldn't fail on true, it should fail on false

--- Source/Types.pas
+++ Source/Types.pas
@@ -41,9 +41,9 @@ type
   Debug = public static class
   public
     [Conditional('DEBUG')]
-    class method Assert(aFail: Boolean; aMessage: String; aFile: String := currentFileName(); aLine: Integer := currentLineNumber());
+    class method Assert(aCheck: Boolean; aMessage: String; aFile: String := currentFileName(); aLine: Integer := currentLineNumber());
     begin
-      if aFail then
+      if not aCheck then
         raise new AssertionException('Assertion failed: '+aMessage+' at '+aFile+'('+aLine+')');
     end;
     class method Throw(s: String);
