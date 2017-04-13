75add482cccc14d8c16456f775a077ac42faf890
marc hoffman <mh@remobjects.com>
2017-04-13 13:28:11 -0400

    TextConvert overloads for passing offset and length

--- Source/TextConvert.pas
+++ Source/TextConvert.pas
@@ -68,11 +68,11 @@ type
     end;
     {$ENDIF}
 
-    class method StringToUTF8(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF8(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
-      if Value = '' then exit new array of Byte(0);
-      var len := Value.Length;
+      if aValue = nil then new ArgumentNullException('aValue is nil');
+      if aValue = '' then exit new array of Byte(0);
+      var len := aValue.Length;
       var arr:= new array of Byte(len*4 + iif(aGenerateBOM,3,0));
       var pos := 0;
       var pos1 := 0;
@@ -84,7 +84,7 @@ type
         pos1 := 3;
       end;
       while pos < len do begin
-        var code := UInt32(Value[pos]);
+        var code := UInt32(aValue[pos]);
         case code of
           0..$7F: begin  // 0xxxxxxx
             arr[pos1] := Byte(code);
@@ -98,7 +98,7 @@ type
           $800..$FFFF: begin //1110xxxx 10xxxxxx 10xxxxxx
             if (($D800 <= code) and (code <= $DBFF)) then begin
               if pos+1>=len then MalformedError;
-              var code1 := UInt32(Value[pos+1]);
+              var code1 := UInt32(aValue[pos+1]);
               if not (($DC00 <= code1) and (code1 <= $DFFF)) then MalformedError;
               //$10000..$1FFFFF,  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
               code := $10000 + (code and $03FF) shl 10 + code1 and $03FF;
@@ -126,16 +126,16 @@ type
       exit r;
     end;
 
-    class method StringToUTF16(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF16(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      exit StringToUTF16LE(Value, aGenerateBOM);
+      exit StringToUTF16LE(aValue, aGenerateBOM);
     end;
 
-    class method StringToUTF16BE(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF16BE(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
-      if Value = '' then exit new array of Byte(0);
-      var len := Value.Length;
+      if aValue = nil then new ArgumentNullException('aValue is nil');
+      if aValue = '' then exit new array of Byte(0);
+      var len := aValue.Length;
       var arr := new array of Byte(len*2 + iif(aGenerateBOM, 2, 0));
       var pos: Integer := 0;
       if aGenerateBOM then begin
@@ -144,7 +144,7 @@ type
         inc(pos,2);
       end;
       for i: Integer := 0 to len-1 do begin
-        var ch := UInt16(Value[i]);
+        var ch := UInt16(aValue[i]);
         arr[pos] := ch shr 8;
         arr[pos+1] := ch and $FF;
         inc(pos,2);
@@ -152,11 +152,11 @@ type
       exit arr;
     end;
 
-    class method StringToUTF16LE(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF16LE(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
-      if Value = '' then exit new array of Byte(0);
-      var len := Value.Length;
+      if aValue = nil then new ArgumentNullException('aValue is nil');
+      if aValue = '' then exit new array of Byte(0);
+      var len := aValue.Length;
       var arr := new array of Byte(len*2 + iif(aGenerateBOM, 2, 0));
       var pos: Integer := 0;
       if aGenerateBOM then begin
@@ -165,7 +165,7 @@ type
         inc(pos,2);
       end;
       for i: Integer := 0 to len-1 do begin
-        var ch := UInt16(Value[i]);
+        var ch := UInt16(aValue[i]);
         arr[pos] := ch and $FF;
         arr[pos+1] := ch shr 8;
         inc(pos,2);
@@ -173,16 +173,16 @@ type
       exit arr;
     end;
 
-    class method StringToUTF32(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF32(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      exit StringToUTF32LE(Value,aGenerateBOM);
+      exit StringToUTF32LE(aValue,aGenerateBOM);
     end;
 
-    class method StringToUTF32BE(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF32BE(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
-      if Value = '' then exit new array of Byte(0);
-      var len := Value.Length;
+      if aValue = nil then new ArgumentNullException('aValue is nil');
+      if aValue = '' then exit new array of Byte(0);
+      var len := aValue.Length;
       var arr :=  new array of Byte(len*4+iif(aGenerateBOM, 4, 0));
       var pos: Integer := 0;
       var pos1:Integer := 0;
@@ -194,10 +194,10 @@ type
         inc(pos1,4);
       end;
       while pos < len do begin
-        var code := UInt32(Value[pos]);
+        var code := UInt32(aValue[pos]);
         if (($D800 <= code) and (code <= $DBFF)) then begin
           if pos+1>=len then MalformedError;
-          var code1 := UInt32(Value[pos+1]);
+          var code1 := UInt32(aValue[pos+1]);
           if not (($DC00 <= code1) and (code1 <= $DFFF)) then MalformedError;
           code := $10000 + (code and $03FF) shl 10 + code1 and $03FF;
           inc(pos);
@@ -214,11 +214,11 @@ type
       exit r;
     end;
 
-    class method StringToUTF32LE(Value: String; aGenerateBOM: Boolean := False): array of Byte;
+    class method StringToUTF32LE(aValue: String; aGenerateBOM: Boolean := False): array of Byte;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
-      if Value = '' then exit new array of Byte(0);
-      var len := Value.Length;
+      if aValue = nil then new ArgumentNullException('aValue is nil');
+      if aValue = '' then exit new array of Byte(0);
+      var len := aValue.Length;
       var arr :=  new array of Byte(len*4+iif(aGenerateBOM, 4, 0));
       var pos: Integer := 0;
       var pos1:Integer := 0;
@@ -230,10 +230,10 @@ type
         inc(pos1,4);
       end;
       while pos < len do begin
-        var code := UInt32(Value[pos]);
+        var code := UInt32(aValue[pos]);
         if (($D800 <= code) and (code <= $DBFF)) then begin
           if pos+1>=len then MalformedError;
-          var code1 := UInt32(Value[pos+1]);
+          var code1 := UInt32(aValue[pos+1]);
           if not (($DC00 <= code1) and (code1 <= $DFFF)) then MalformedError;
           code := $10000 + (code and $03FF) shl 10 + code1 and $03FF;
           inc(pos);
@@ -255,32 +255,32 @@ type
       result := UTF8ToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF8ToString(Value: array of Byte; aOffset: Integer; len: Integer): String;
+    class method UTF8ToString(aValue: array of Byte; aOffset: Integer; len: Integer): String;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
+      if aValue = nil then new ArgumentNullException('aValue is nil');
       if len = 0 then exit '';
       var str:= new StringBuilder(len);
       var pos := aOffset;
       // skip BOM
       if len>2 then begin
-        if (Value[0] = $EF) and
-           (Value[1] = $BB) and
-           (Value[2] = $BF) then pos := 3;
+        if (aValue[0] = $EF) and
+           (aValue[1] = $BB) and
+           (aValue[2] = $BF) then pos := 3;
       end;
       while pos < len do begin
-        var ch := Value[pos];
+        var ch := aValue[pos];
         {$REGION 4 bytes Char}
         if ch and $F0 = $F0 then begin
           //   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
           if pos+4 > len then BadUTF8Error;
           var code := (((((
                         UInt32(ch and $7) shl 6 +
-                        (UInt32(Value[pos+1]) and $3F)) shl 6)+
-                        (UInt32(Value[pos+2]) and $3F)) shl 6)+
-                        (UInt32(Value[pos+3]) and $3F));
+                        (UInt32(aValue[pos+1]) and $3F)) shl 6)+
+                        (UInt32(aValue[pos+2]) and $3F)) shl 6)+
+                        (UInt32(aValue[pos+3]) and $3F));
           if (code < $10000) or (code>$1FFFFF) then MalformedError;
           {$IFDEF DEBUG_UTF8}
-          DebugLog(Value,pos,4,Char(code));
+          DebugLog(aValue,pos,4,Char(code));
           {$ENDIF}
           var code1 := code - $10000;
           str.Append(Char($D800 or (code1 shr 10)));
@@ -294,11 +294,11 @@ type
           if pos+3 > len then BadUTF8Error;
           var code := ((
                         UInt32(ch and $F) shl 6 +
-                        (UInt32(Value[pos+1]) and $3F)) shl 6)+
-                        (UInt32(Value[pos+2]) and $3F);
+                        (UInt32(aValue[pos+1]) and $3F)) shl 6)+
+                        (UInt32(aValue[pos+2]) and $3F);
           if (code < $800) or (code > $FFFF) then MalformedError;
           {$IFDEF DEBUG_UTF8}
-          DebugLog(Value,pos,3,Char(code));
+          DebugLog(aValue,pos,3,Char(code));
           {$ENDIF}
           str.Append(Char(code));
           inc(pos,3);
@@ -310,10 +310,10 @@ type
           if pos+2 > len then BadUTF8Error;
           var code :=
                       UInt32(ch and $1F) shl 6 +
-                      (UInt32(Value[pos+1]) and $3F);
+                      (UInt32(aValue[pos+1]) and $3F);
           if (code < $80) or (code >$7FF) then MalformedError;
           {$IFDEF DEBUG_UTF8}
-          DebugLog(Value,pos,2,Char(code));
+          DebugLog(aValue,pos,2,Char(code));
           {$ENDIF}
           str.Append(Char(code));
           inc(pos,2);
@@ -325,7 +325,7 @@ type
           var code := ch;
           if (code < $0)  or (code > $7F) then MalformedError;
           {$IFDEF DEBUG_UTF8}
-          DebugLog(Value,pos,1,Char(code));
+          DebugLog(aValue,pos,1,Char(code));
           {$ENDIF}
           str.Append(Char(code));
           inc(pos,1);
@@ -344,10 +344,10 @@ type
       result := UTF16ToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF16ToString(Value: array of Byte; aOffset: Integer; len: Integer): String; inline;
+    class method UTF16ToString(aValue: array of Byte; aOffset: Integer; len: Integer): String; inline;
     begin
       {$WARNING need to check for endianess}
-      exit UTF16LEToString(Value);
+      exit UTF16LEToString(aValue);
     end;
 
     class method UTF16BEToString(aValue: array of Byte): String; inline;
@@ -355,20 +355,20 @@ type
       result := UTF16BEToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF16BEToString(Value: array of Byte; aOffset: Integer; len: Integer): String;
+    class method UTF16BEToString(aValue: array of Byte; aOffset: Integer; len: Integer): String;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
+      if aValue = nil then new ArgumentNullException('aValue is nil');
       if len = 0 then exit '';
       var len2 := len/2;
       if len - len2*2 = 1 then BadArray;
       var str := new StringBuilder(len2);
       var start := aOffset;
       if len>1 then begin
-        if (Value[0] = $FE) and (Value[1] = $FF) then
+        if (aValue[0] = $FE) and (aValue[1] = $FF) then
           inc(start,2);
       end;
       for i: Integer :=start to len2 - 1 do
-        str.Append(Char(Value[i*2] shl 8 + Value[i*2+1]));
+        str.Append(Char(aValue[i*2] shl 8 + aValue[i*2+1]));
       exit str.ToString;
     end;
 
@@ -377,20 +377,20 @@ type
       result := UTF16LEToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF16LEToString(Value: array of Byte; aOffset: Integer; len: Integer): String;
+    class method UTF16LEToString(aValue: array of Byte; aOffset: Integer; len: Integer): String;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
+      if aValue = nil then new ArgumentNullException('aValue is nil');
       if len = 0 then exit '';
       var len2 := len/2;
       if len - len2*2 = 1 then BadArray;
       var str := new StringBuilder(len2);
       var start := aOffset;
       if len>1 then begin
-        if (Value[0] = $FF) and (Value[1] = $FE) then
+        if (aValue[0] = $FF) and (aValue[1] = $FE) then
           inc(start,2);
       end;
       for i: Integer := start to len2 - 1 do
-        str.Append(Char(Value[i*2] + Value[i*2+1] shl 8));
+        str.Append(Char(aValue[i*2] + aValue[i*2+1] shl 8));
       exit str.ToString;
     end;
 
@@ -399,10 +399,10 @@ type
       result := UTF32ToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF32ToString(Value: array of Byte; aOffset: Integer; len: Integer): String; inline;
+    class method UTF32ToString(aValue: array of Byte; aOffset: Integer; len: Integer): String; inline;
     begin
       {$WARNING need to check for endianess}
-      exit UTF32LEToString(Value);
+      exit UTF32LEToString(aValue);
     end;
 
     class method UTF32BEToString(aValue: array of Byte): String; inline;
@@ -410,24 +410,24 @@ type
       result := UTF32BEToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF32BEToString(Value: array of Byte; aOffset: Integer; len: Integer): String;
+    class method UTF32BEToString(aValue: array of Byte; aOffset: Integer; len: Integer): String;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
+      if aValue = nil then new ArgumentNullException('aValue is nil');
       if len - (len /4)*4 > 0 then BadArray;
       var str:= new StringBuilder;
       var idx := aOffset;
       // ignore BOM
       if len>3 then begin
-        if (Value[0]=$00) and
-           (Value[1]=$00) and
-           (Value[2]=$FE) and
-           (Value[3]=$FF) then inc(idx,4) ;
+        if (aValue[0]=$00) and
+           (aValue[1]=$00) and
+           (aValue[2]=$FE) and
+           (aValue[3]=$FF) then inc(idx,4) ;
       end;
       while idx < len do begin
-        var code:UInt32 :=  Value[idx] shl 24 +
-                            Value[idx+1] shl 16 +
-                            Value[idx+2] shl 8 +
-                            Value[idx+3] ;
+        var code:UInt32 :=  aValue[idx] shl 24 +
+                            aValue[idx+1] shl 16 +
+                            aValue[idx+2] shl 8 +
+                            aValue[idx+3] ;
         if code < $10000 then begin
           //$0000-$FFFF
           str.Append(Char(code));
@@ -451,24 +451,24 @@ type
       result := UTF32LEToString(aValue, 0, length(aValue));
     end;
 
-    class method UTF32LEToString(Value: array of Byte; aOffset: Integer; len: Integer): String;
+    class method UTF32LEToString(aValue: array of Byte; aOffset: Integer; len: Integer): String;
     begin
-      if Value = nil then new ArgumentNullException('Value is nil');
+      if aValue = nil then new ArgumentNullException('aValue is nil');
       if len - (len /4)*4 > 0 then BadArray;
       var str:= new StringBuilder;
       var idx := aOffset;
       // ignore BOM
       if len>3 then begin
-        if (Value[0]=$FF) and
-           (Value[1]=$FE) and
-           (Value[2]=$00) and
-           (Value[3]=$00) then inc(idx,4) ;
+        if (aValue[0]=$FF) and
+           (aValue[1]=$FE) and
+           (aValue[2]=$00) and
+           (aValue[3]=$00) then inc(idx,4) ;
       end;
       while idx < len do begin
-        var code:UInt32 :=  Value[idx] +
-                            Value[idx+1] shl 8 +
-                            Value[idx+2] shl 16 +
-                            Value[idx+3] shl 24;
+        var code:UInt32 :=  aValue[idx] +
+                            aValue[idx+1] shl 8 +
+                            aValue[idx+2] shl 16 +
+                            aValue[idx+3] shl 24;
         if code < $10000 then begin
           //$0000-$FFFF
           str.Append(Char(code));
@@ -487,19 +487,19 @@ type
       exit str.ToString;
     end;
 
-    class method StringToASCII(Value: String): array of Byte;
+    class method StringToASCII(aValue: String): array of Byte;
     begin
       {$IFDEF WINDOWS}
-      var len := rtl.WideCharToMultiByte(rtl.CP_ACP, 0, Value.FirstChar, Value.Length, nil, 0, nil, nil);
+      var len := rtl.WideCharToMultiByte(rtl.CP_ACP, 0, aValue.FirstChar, aValue.Length, nil, 0, nil, nil);
       result := new Byte[len];
-      rtl.WideCharToMultiByte(rtl.CP_ACP, 0, Value.FirstChar, Value.Length, rtl.LPSTR(@result[0]), len, nil, nil);
+      rtl.WideCharToMultiByte(rtl.CP_ACP, 0, aValue.FirstChar, aValue.Length, rtl.LPSTR(@result[0]), len, nil, nil);
       {$ELSEIF ANDROID}
-      var b := StringToUTF8(Value, false);
+      var b := StringToUTF8(aValue, false);
       result := new Byte[b.Length];
       rtl.memcpy(@result[0], @b[0], b.Length);
       {$ELSE}
       var lNewData: ^AnsiChar := nil;
-      var lNewLen: rtl.size_t := iconv_helper(TextConvert.fUTF16ToCurrent, ^AnsiChar(Value.FirstChar), Value.Length * 2, Value.Length + 5, out lNewData);
+      var lNewLen: rtl.size_t := iconv_helper(TextConvert.fUTF16ToCurrent, ^AnsiChar(aValue.FirstChar), aValue.Length * 2, aValue.Length + 5, out lNewData);
 
       if lNewLen <> -1  then begin
         result := new Byte[lNewLen];
@@ -519,9 +519,9 @@ type
       if aValue = nil then exit nil;
       if aCount = 0 then exit '';
       {$IFDEF WINDOWS}
-      var len := rtl.MultiByteToWideChar(rtl.CP_ACP, 0, rtl.LPCCH(@Value[aOffset]), aCount, nil, 0);
+      var len := rtl.MultiByteToWideChar(rtl.CP_ACP, 0, rtl.LPCCH(@aValue[aOffset]), aCount, nil, 0);
       result := String.AllocString(len);
-      rtl.MultiByteToWideChar(rtl.CP_ACP, 0, rtl.LPCCH(@aValue[aOffset]), aCharCount, @result.fFirstChar, len);
+      rtl.MultiByteToWideChar(rtl.CP_ACP, 0, rtl.LPCCH(@aValue[aOffset]), aCount, @result.fFirstChar, len);
       {$ELSEIF ANDROID}
       exit TextConvert.UTF8ToString(aValue, aOffset, aCount);
       {$ELSE}
