f90adf3e62ecf79f2db9cea5ba52f5fdbc8c10a9
evgeny-k <git@evgenysoft.com>
2017-06-20 15:03:17 +0300

    test for `Math.Power(double, int) calculates incorrectly` was added

---
+++ Tests/Math_Power.pas
@@ -0,0 +1,27 @@
+namespace Elements.RTL2.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit;
+
+type
+  Math_Power = public class(Test)
+  public
+
+    method calc;
+    begin
+      var s1,s2: Double;
+      var sValue:=10.0;
+      s1 :=sValue*sValue;
+      s2 := Math.Pow(sValue,2);
+      Assert.AreEqual(s1, s2);
+      s1 := 1/(sValue*sValue);
+      s2 := Math.Pow(sValue,-2);
+      Assert.AreEqual(s1, s2);
+      s1 := 1;
+      s2 := Math.Pow(sValue,0);
+      Assert.AreEqual(s1, s2);
+    end;
+
+  end;
+
+end.
