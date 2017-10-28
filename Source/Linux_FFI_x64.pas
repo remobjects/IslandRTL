05c4fc6688b1cfcebabe07ca26b04d6bd29e88f4
Carlo Kok <ck@remobjects.com>
2017-10-28 17:01:38 +0200

    0: Fix for Linux

--- Source/Linux_FFI_x64.pas
+++ Source/Linux_FFI_x64.pas
@@ -239,7 +239,7 @@ type
       if aParamMode in [ArgumentMode.Out, ArgumentMode.Var] then begin
         //StoreReg(NativeUInt(@aParam));
         if aParamType.IsValueType then begin
-          var lAlloc := Utilities.NewInstance(aParamType.RTTI, aParamType.SizeOfType + sizeOf(^Void)); // header
+          var lAlloc := DefaultGC.New(aParamType.RTTI, aParamType.SizeOfType + sizeOf(^Void)); // header
           memcpy(@(^^Void(lAlloc)[1]),@(^^Void(InternalCalls.Cast(aParam))[1]), aParamType.SizeOfType);
           StoreReg(UInt64(@(^^Void(lAlloc)[1])));
           fVarData[aParamIndex] := InternalCalls.Cast<Object>(lAlloc);
