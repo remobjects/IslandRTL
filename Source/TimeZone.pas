465e39736c02d35d1e10ca91f7ae8b11e29df409
Diego Navarro <dinavarro@gmail.com>
2017-11-10 18:08:52 +0100

    Add TimeZone class
    WIP, pending test on Android and improve Linux TimeZoneByName

---
+++ Source/TimeZone.pas
@@ -0,0 +1,164 @@
+﻿namespace RemObjects.Elements.System;
+
+interface
+
+type
+  TimeZone = public class
+  private
+    fID: String;
+    fOffsetToUTC: Int64;
+    class method EnumTimeZones: not nullable sequence of String;
+    class method get_LocalTimeZone: not nullable TimeZone;
+    class method get_UtcTimeZone: not nullable TimeZone;
+    class method get_TimeZoneWithName(aName: String): nullable TimeZone;
+    class method get_TimeZoneNames: not nullable sequence of String;
+    {$IFDEF LINUX}
+    class method GetZoneNames(aZonesDir: String; aList: List<String>; aZone: String);
+    const DefaultZonesDir = '/usr/share/zoneinfo';
+    const DefaultVarName = 'TZ';
+    {$ENDIF}
+  protected
+    constructor(aID: String; aOffsetToUTC: Integer);
+  public
+    class property Local: not nullable TimeZone read get_LocalTimeZone;
+    class property Utc: not nullable TimeZone read get_UtcTimeZone;
+    class property TimeZoneByName[aName: String]: nullable TimeZone read get_TimeZoneWithName;
+    class property TimeZoneNames: not nullable sequence of String read get_TimeZoneNames;
+
+    //property Name: String read nil; {$WARNING Not Implemented}
+    property Identifier: String read fID;
+    property OffsetToUTC: Int64 read fOffsetToUTC;
+  end;
+
+implementation
+
+class method TimeZone.get_LocalTimeZone: not nullable TimeZone;
+begin
+  {$IFDEF WINDOWS}
+  var lZoneInfo: rtl.LPTIME_ZONE_INFORMATION;
+  var lDayLight := rtl.GetTimeZoneInformation(lZoneInfo) = rtl.TIME_ZONE_ID_DAYLIGHT;
+  var lOffset := if lDayLight then (lZoneInfo^.DaylightBias +  lZoneInfo^.Bias) * 60 else lZoneInfo^.Bias * 60;
+  var lID := String.FromPChar(lZoneInfo^.StandardName);
+  result := new TimeZone(lID, lOffset);
+  {$ELSEIF POSIX OR ANDROID}
+  var lTime: rtl.time_t := rtl.time(nil);
+  var lTimeZone: ^rtl.__struct_tm;
+  lTimeZone := rtl.localtime(@lTime);  
+  result := new TimeZone(String.FromPAnsiChars(lTimeZone^.tm_zone), lTimeZone^.tm_gmtoff div 3600);
+  {$ENDIF}
+end;
+
+class method TimeZone.get_UtcTimeZone: not nullable TimeZone;
+begin
+  result := new TimeZone('UTC', 0);
+end;
+
+constructor TimeZone(aID: String; aOffsetToUTC: Integer);
+begin
+  fID := aID;
+  fOffsetToUTC := aOffsetToUTC;
+end;
+
+class method TimeZone.get_TimeZoneNames: not nullable sequence of String;
+begin
+  result := EnumTimeZones;
+end;
+
+class method TimeZone.EnumTimeZones: not nullable sequence of String;
+begin
+  {$IFDEF WINDOWS}
+  var lKey := 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Time Zones'.ToCharArray(true);
+  var lNewKey: rtl.HKEY;
+  var lSubKeys: rtl.DWORD;
+  var lMaxLength: rtl.DWORD;
+
+  var lList: not nullable List<String> := new List<String>;
+  if rtl.RegOpenKeyEx(rtl.HKEY_LOCAL_MACHINE, @lKey[0], 0, rtl.KEY_READ or rtl.KEY_WOW64_RES, @lNewKey) <> rtl.ERROR_SUCCESS then 
+    raise new Exception('Can not open TimeZone registry key');
+
+  if rtl.RegQueryInfoKey(lNewKey, nil, nil, nil, @lSubKeys, @lMaxLength, nil, nil, nil, nil, nil, nil) <> rtl.ERROR_SUCCESS then
+    raise new Exception('Can not get information from TimeZone registry key');
+  var lSubName := new Char[lMaxLength];
+  var lWritten: rtl.DWORD;
+  for i: Integer := 0 to lSubKeys - 1 do begin
+    rtl.RegEnumKeyEx(lNewKey, i, @lSubName[0], @lWritten, nil, nil, nil, nil);
+    lList.Add(new String(lSubName, lWritten));
+  end;
+  result := lList;
+  {$ELSEIF POSIX OR ANDROID}
+  var lList: not nullable List<String> := new List<String>();
+  GetZoneNames(DefaultZonesDir, lList, '');
+  result := lList;
+  {$ENDIF}
+end;
+
+{$IFDEF LINUX}
+class method TimeZone.GetZoneNames(aZonesDir: String; aList: List<String>; aZone: String);
+begin
+  var lBytes := aZonesDir.ToAnsiChars(true);
+  var lDir: ^rtl.DIR := rtl.opendir(@lBytes[0]);
+  if lDir = nil then
+    raise new Exception('Can not get TimeZones: ' + aZonesDir);
+
+  var lContent: ^rtl.__struct_dirent;
+  var lName: String;
+  lContent := rtl.readdir(lDir);
+  while lContent <> nil do begin
+    if lContent^.d_type = rtl.DT_DIR then begin
+      lName := String.FromPAnsiChars(lContent^.d_name);
+      if (lName <> '.') and (lName <> '..') and (lName <> 'posix') and
+        (lName <> 'SystemV') and (lName <> 'right') then GetZoneNames(aZonesDir + '/' + lName, aList, lName);
+    end
+    else begin
+      lName := String.FromPAnsiChars(lContent^.d_name);
+      if lName.IndexOf('.') < 0 then begin
+        if aZone <> '' then aList.Add(aZone + '/' + lName)
+        else aList.Add(lName);
+      end;
+    end;
+    lContent := rtl.readdir(lDir);
+  end;
+  rtl.closedir(lDir);
+end;
+{$ENDIF}
+
+class method TimeZone.get_TimeZoneWithName(aName: String): nullable TimeZone;
+begin
+  {$IFDEF WINDOWS}
+  var lKey := ('SOFTWARE\Microsoft\Windows NT\CurrentVersion\Time Zones\' + aName).ToCharArray(true);
+  var lNewKey: rtl.HKEY;
+
+  if rtl.RegOpenKeyEx(rtl.HKEY_LOCAL_MACHINE, @lKey[0], 0, rtl.KEY_READ or rtl.KEY_WOW64_RES, @lNewKey) <> rtl.ERROR_SUCCESS then 
+    raise new Exception('Can not open TimeZone registry key');
+
+  var lType: rtl.DWORD;
+  var lBuffer: rtl.TIME_ZONE_INFORMATION;
+  var lBufSize: rtl.DWORD := sizeOf(lBuffer);
+  var lKeyChars := 'TZI'.ToCharArray(true);
+  if rtl.RegGetValue(lNewKey, nil, @lKeyChars[0], rtl.RRF_RT_ANY, @lType, @lBuffer, @lBufSize) <> rtl.ERROR_SUCCESS then
+    raise new Exception('Can not retrieve timezone info for ' + aName);
+  var lDayLight := lBuffer.DaylightDate.wMonth <> 0;
+  var lOffset := if lDayLight then (lBuffer.DaylightBias +  lBuffer.Bias) * 60 else lBuffer.Bias * 60;
+  result := new TimeZone(aName, lOffset);
+  {$ELSEIF LINUX}
+  var lNameBytes := DefaultVarName.ToAnsiChars(true);
+  var lValueBytes := aName.ToAnsiChars(true);
+  var lOldValue := rtl.getenv(@lNameBytes[0]);
+  try
+    rtl.setenv(@lNameBytes[0], @lValueBytes[0], 1);
+    var lTime: rtl.time_t := rtl.time(nil);
+    var lTimeZone: ^rtl.__struct_tm;
+    lTimeZone := rtl.localtime(@lTime);  
+    result := new TimeZone(aName, lTimeZone^.tm_gmtoff div 3600);
+  finally
+    if lOldValue = nil then begin
+      var lBytesNoValue := ''.ToAnsiChars(true);
+      rtl.setenv(@lNameBytes[0], @lBytesNoValue[0], 1)
+    end
+    else
+      rtl.setenv(@lNameBytes[0], lOldValue, 1);
+  end;
+  {$ENDIF}
+end;
+
+end.
