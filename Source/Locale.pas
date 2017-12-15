1cf1e75a1725bb027104a7774f5dac250c3eceee
Diego Navarro <diegon@remobjects.com>
2017-12-15 20:06:38 +0100

    Adding thousands separator, currency symbol and decimal separator to Island Locale.

--- Source/Locale.pas
+++ Source/Locale.pas
@@ -6,8 +6,9 @@ type
   NumberFormatInfo = public class
   public
     property DecimalSeparator: Char;
-    property DecimalDigits: Integer;
-    //constructor(aDecimalSeparator: Char; aDecimalDigits: Integer);
+    property ThousandsSeparator: Char;
+    property Currency: String;
+    constructor(aDecimalSeparator: Char; aThousandsSeparator: Char; aCurrency: String);
   end;
 
   PlatformLocale = {$IF WINDOWS}rtl.LCID{$ELSEIF LINUX}rtl.locale_t{$ENDIF};
@@ -33,13 +34,49 @@ implementation
 constructor Locale(aLocaleID: PlatformLocale);
 begin
   fLocaleID := aLocaleID;
+  var lCurrency: String := '';
+  var lThousandsSep: Char := #0;
+  var lDecimalSep: Char := #0;
+  {$IF WINDOWS}
+  var lBuffer := new Char[50];
+  var lTotal := rtl.GetLocaleInfo(fLocaleID, rtl.LOCALE_SCURRENCY, @lBuffer[0], lBuffer.Length);
+  lCurrency := String.FromPChar(@lBuffer[0], lTotal - 1);
+  lTotal := rtl.GetLocaleInfo(fLocaleID, rtl.LOCALE_SDECIMAL, @lBuffer[0], lBuffer.Length);
+  var lTemp := String.FromPChar(@lBuffer[0], lTotal - 1);
+  if lTemp.Length > 0 then
+     lDecimalSep := lTemp[0];
+  lTotal := rtl.GetLocaleInfo(fLocaleID, rtl.LOCALE_STHOUSAND, @lBuffer[0], lBuffer.Length);
+  lTemp := String.FromPChar(@lBuffer[0], lTotal).SubString(0, 1);
+  if lTemp.Length > 0 then
+  lThousandsSep := lTemp[0];  
+  {$ELSEIF LINUX}
+  var lTemp := rtl.nl_langinfo_l(rtl.THOUSANDS_SEP, fLocaleID);
+  if lTemp <> nil then begin
+    var lTempString := String.FromPAnsiChars(lTemp);
+    if lTempString.Length > 0 then
+      lThousandsSep := lTempString[0];
+  end;
+  lTemp := rtl.nl_langinfo_l(rtl.RADIXCHAR, fLocaleID);
+  if lTemp <> nil then begin
+    var lTempString := String.FromPAnsiChars(lTemp);
+    if lTempString.Length > 0 then
+      lDecimalSep := lTempString[0];
+  end;
+  lTemp := rtl.nl_langinfo_l(rtl.CRNCYSTR, fLocaleID);
+  if lTemp <> nil then begin
+    var lTempString := String.FromPAnsiChars(lTemp) as not nullable;
+    if lTempString.Length > 1 then
+      lCurrency := lTempString.SubString(1);
+  end;
+  {$ENDIF}
+  fNumberFormat := new NumberFormatInfo(lDecimalSep, lThousandsSep, lCurrency);
 end;
 
 method Locale.GetIdentifier: not nullable String;
 begin
   {$IF WINDOWS}
   var lName := new Char[rtl.LOCALE_NAME_MAX_LENGTH];
-  var lLength := rtl.LCIDToLocaleName(fLCID, @lName[0], rtl.LOCALE_NAME_MAX_LENGTH, 0);
+  var lLength := rtl.LCIDToLocaleName(fLocaleID, @lName[0], rtl.LOCALE_NAME_MAX_LENGTH, 0);
   if lLength = 0 then 
     raise new Exception("Error getting locale name");
   result := String.FromPChar(@lName[0], lLength) as not nullable;
@@ -56,7 +93,7 @@ begin
   {$IF WINDOWS}
   result := new Locale(rtl.LOCALE_INVARIANT);
   {$ELSEIF LINUX}
-  var lInvariant := 'en.UTF8'.ToAnsiChars(true);
+  var lInvariant := 'en_US.utf8'.ToAnsiChars(true);
   result := new Locale(rtl.newLocale(rtl.LC_ALL_MASK, @lInvariant[0], nil));
   {$ENDIF}
 end;
@@ -69,10 +106,19 @@ begin
   var lDefaultName := Environment.GetEnvironmentVariable('LANG');
   if lDefaultName = '' then
     raise new Exception('Can not get default locale');
+  lDefaultName := lDefaultName.Replace('.UTF-8', '.utf8');
   var lName := lDefaultName.ToAnsiChars(true);
   var lLocale := rtl.newlocale(rtl.LC_ALL_MASK, @lName[0], nil);
   result := new Locale(lLocale);
+  writeln('end of GetCurrent');
   {$ENDIF}
+end;
+
+constructor NumberFormatInfo(aDecimalSeparator: Char; aThousandsSeparator: Char; aCurrency: String);
+begin
+  DecimalSeparator := aDecimalSeparator;
+  ThousandsSeparator := aThousandsSeparator;
+  Currency := aCurrency;
 end;
 
 end.
