28269da83a2ed147de9752c2e2395783e1e501db
Diego Navarro <dinavarro@gmail.com>
2018-02-06 18:55:39 +0100

    Getting more locale info (month names, day names, ..) on Windows (WIP)

--- Source/Locale.pas
+++ Source/Locale.pas
@@ -11,6 +11,54 @@ type
     constructor(aDecimalSeparator: Char; aThousandsSeparator: Char; aCurrency: String);
   end;
 
+  DateTimeFormatInfo = public class
+  private
+    fShortDayNames := new String[7];
+    fLongDayNames: array of String;
+    fShortMonthNames: array of String;
+    fLongMonthNames: array of String;
+    fDateSeparator: String;
+    fTimeSeparator: String;
+    fAMString: String;
+    fPMString: String;
+    fShortDatePattern: String;
+    fLongDatePattern: String;
+    fShortTimePattern: String;
+    fLongTimePattern: String;
+    fIsReadOnly: Boolean;
+    method SetShortDayNames(aValue: array of String);
+    method SetLongDayNames(aValue: array of String);
+    method SetShortMonthNames(aValue: array of String);
+    method SetLongMonthNames(aValue: array of String);
+    method SetDateSeparator(aValue: String);
+    method SetTimeSeparator(aValue: String);
+    method SetAMString(aValue: String);
+    method SetPMString(aValue: String);
+    method SetShortTimePattern(aValue: String);
+    method SetLongTimePattern(aValue: String);
+    method SetShortDatePattern(aValue: String);
+    method SetLongDatePattern(aValue: String);
+    method CheckReadOnly;
+    {$IF WINDOWS}
+    method GetStringFromLocale(aLocaleID: PlatformLocale; aLocaleItem: rtl.LCTYPE): String;
+    {$ENDIF}
+  public
+    constructor(aLocale: PlatformLocale; aIsReadonly: Boolean := false);
+    property ShortDayNames: array of String read fShortDayNames write SetShortDayNames;
+    property LongDayNames: array of String read fLongDayNames write SetLongDayNames;
+    property ShortMonthNames: array of String read fShortMonthNames write SetShortMonthNames;
+    property LongMonthNames: array of String read fLongMonthNames write SetLongMonthNames;
+    property DateSeparator: String read fDateSeparator write SetDateSeparator;
+    property TimeSeparator: String read fTimeSeparator write SetTimeSeparator;
+    property AMString: String read fAMString write SetAMString;
+    property PMString: String read fPMString write SetPMString;
+    property ShortTimePattern: String read fShortTimePattern write SetShortTimePattern;
+    property LongTimePattern: String read fLongTimePattern write SetLongTimePattern;
+    property ShortDatePattern: String read fShortDatePattern write SetShortDatePattern;
+    property LongDatePattern: String read fLongDatePattern write SetLongDatePattern;
+    property IsReadOnly: Boolean read fIsReadOnly;
+  end;
+
   PlatformLocale = {$IF WINDOWS}rtl.LCID{$ELSEIF LINUX AND NOT ANDROID}rtl.locale_t{$ELSEIF ANDROID OR WEBASSEMBLY}String{$ENDIF};
 
   Locale = public class
@@ -38,6 +86,84 @@ type
 
 implementation
 
+method DateTimeFormatInfo.SetShortDayNames(aValue: array of String);
+begin
+  CheckReadOnly;
+  fShortDayNames := aValue;
+end;
+
+method DateTimeFormatInfo.SetLongDayNames(aValue: array of String);
+begin
+  CheckReadOnly;
+  fLongDayNames := aValue;
+end;
+
+method DateTimeFormatInfo.SetShortMonthNames(aValue: array of String);
+begin
+  CheckReadOnly;
+  fShortMonthNames := aValue;
+end;
+
+method DateTimeFormatInfo.SetLongMonthNames(aValue: array of String);
+begin
+  CheckReadOnly;
+  fLongMonthNames := aValue;
+end;
+
+method DateTimeFormatInfo.SetDateSeparator(aValue: String);
+begin
+  CheckReadOnly;
+  fDateSeparator := aValue;
+end;
+
+method DateTimeFormatInfo.SetTimeSeparator(aValue: String);
+begin
+  CheckReadOnly;
+  fTimeSeparator := aValue;
+end;
+
+method DateTimeFormatInfo.SetAMString(aValue: String);
+begin
+  CheckReadOnly;
+  fAMString := aValue;
+end;
+
+method DateTimeFormatInfo.SetPMString(aValue: String);
+begin
+  CheckReadOnly;
+  fPMString := aValue;
+end;
+
+method DateTimeFormatInfo.SetShortTimePattern(aValue: String);
+begin
+  CheckReadOnly;
+  fShortTimePattern := aValue;
+end;
+
+method DateTimeFormatInfo.SetLongTimePattern(aValue: String);
+begin
+  CheckReadOnly;
+  fLongTimePattern := aValue;
+end;
+
+method DateTimeFormatInfo.SetShortDatePattern(aValue: String);
+begin
+  CheckReadOnly;
+  fShortDatePattern := aValue;
+end;
+
+method DateTimeFormatInfo.SetLongDatePattern(aValue: String);
+begin
+  CheckReadOnly;
+  fLongDatePattern := aValue;
+end;
+
+method DateTimeFormatInfo.CheckReadOnly;
+begin
+  if IsReadOnly then
+    raise new Exception('Can not modify this DateTimeFormatInfo instance');
+end;
+
 constructor Locale(aLocaleID: PlatformLocale);
 begin
   fLocaleID := aLocaleID;
@@ -55,7 +181,7 @@ begin
   lTotal := rtl.GetLocaleInfo(fLocaleID, rtl.LOCALE_STHOUSAND, @lBuffer[0], lBuffer.Length);
   lTemp := String.FromPChar(@lBuffer[0], lTotal).SubString(0, 1);
   if lTemp.Length > 0 then
-  lThousandsSep := lTemp[0];  
+  lThousandsSep := lTemp[0];
   {$ELSEIF LINUX AND NOT ANDROID}
   var lTemp := rtl.nl_langinfo_l(rtl.THOUSANDS_SEP, fLocaleID);
   if lTemp <> nil then begin
@@ -80,7 +206,7 @@ begin
   var lStatus: UErrorCode;
   var lBuffer := new Char[30];
   var lTotal: Int32;
-  var lFormatSettings := ICUHelper.UNumOpen(UNumberFormatStyle.UNUM_CURRENCY, nil, 0, @fLocaleID.ToAnsiChars(true)[0], @lParseError, @lStatus);  
+  var lFormatSettings := ICUHelper.UNumOpen(UNumberFormatStyle.UNUM_CURRENCY, nil, 0, @fLocaleID.ToAnsiChars(true)[0], @lParseError, @lStatus);
   lTotal := ICUHelper.UNumGetSymbol(lFormatSettings, UNumberFormatSymbol.UNUM_DECIMAL_SEPARATOR_SYMBOL, @lBuffer[0], lBuffer.Length, @lStatus);
   lDecimalSep := String.FromPChar(@lBuffer[0], lTotal)[0];
   lTotal := ICUHelper.UNumGetSymbol(lFormatSettings, UNumberFormatSymbol.UNUM_GROUPING_SEPARATOR_SYMBOL, @lBuffer[0], lBuffer.Length, @lStatus);
@@ -108,7 +234,7 @@ begin
   {$IF WINDOWS}
   var lName := new Char[rtl.LOCALE_NAME_MAX_LENGTH];
   var lLength := rtl.LCIDToLocaleName(fLocaleID, @lName[0], rtl.LOCALE_NAME_MAX_LENGTH, 0);
-  if lLength = 0 then 
+  if lLength = 0 then
     raise new Exception("Error getting locale name");
   result := String.FromPChar(@lName[0], lLength) as not nullable;
   {$ELSEIF LINUX AND NOT ANDROID}
@@ -166,7 +292,7 @@ begin
     end;
     fCurrent := new Locale(lDefaultName);
     {$ELSEIF WEBASSEMBLY}
-    var lHandle := WebAssemblyCalls.GetCurrentLocale;    
+    var lHandle := WebAssemblyCalls.GetCurrentLocale;
     fCurrent := new Locale(WebAssembly.GetStringFromHandle(lHandle, true));
     {$ENDIF}
   end;
@@ -179,5 +305,40 @@ begin
   ThousandsSeparator := aThousandsSeparator;
   Currency := aCurrency;
 end;
+
+constructor DateTimeFormatInfo(aLocale: PlatformLocale; aIsReadonly: Boolean := false);
+begin
+  {$IF WINDOWS}
+  fIsReadOnly := aIsReadonly;
+  for i: Integer := 0 to 6 do begin
+    fShortDayNames[i] := GetStringFromLocale(aLocale, rtl.LOCALE_SABBREVDAYNAME1 + i);
+    fLongDayNames[i] := GetStringFromLocale(aLocale, rtl.LOCALE_SDAYNAME1 + i);
+  end;
+  for
+
+  for i: Integer := 0 to 11 do begin
+    fShortMonthNames[i] := GetStringFromLocale(aLocale, rtl.LOCALE_SABBREVMONTHNAME1 + i);
+    fLongMonthNames[i] := GetStringFromLocale(aLocale, rtl.LOCALE_SMONTHNAME1 + i);
+  end;
+
+  fDateSeparator := GetStringFromLocale(aLocale, rtl.LOCALE_SDATE);
+  fTimeSeparator := GetStringFromLocale(aLocale, rtl.LOCALE_STIME);
+  fAMString := GetStringFromLocale(aLocale, rtl.LOCALE_S1159);
+  fPMString := GetStringFromLocale(aLocale, rtl.LOCALE_S2359);
+  fShortDatePattern := GetStringFromLocale(aLocale, rtl.LOCALE_SSHORTDATE);
+  fLongDatePattern := GetStringFromLocale(aLocale, rtl.LOCALE_SLONGDATE);
+  //fShortTimePattern := GetStringFromLocale(aLocale, rtl.LOCALE_SSHORTTIME);
+  //fLongTimePattern := GetStringFromLocale(aLocale, rtl.LOCALE_SLO
+  {$ENDIF}
+end;
+
+{$IF WINDOWS}
+method DateTimeFormatInfo.GetStringFromLocale(aLocaleID: PlatformLocale; aLocaleItem: rtl.LCTYPE): String;
+begin
+  var lBuffer := new Char[50];
+  var lTotal := rtl.GetLocaleInfo(aLocaleID, aLocaleItem, @lBuffer[0], lBuffer.Length);
+  result := String.FromPChar(@lBuffer[0], lTotal - 1);
+end;
+{$ENDIF}
 
 end.
