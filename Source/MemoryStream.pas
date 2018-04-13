e9512cdd9a3abfbd0ab0fd80bfce1ae3a548582e
Carlo Kok <ck@remobjects.com>
2018-04-13 15:08:26 +0200

    79948: Span/memory types

--- Source/MemoryStream.pas
+++ Source/MemoryStream.pas
@@ -22,8 +22,8 @@ type
     property CanSeek: Boolean read IsValid; override;
     property CanWrite: Boolean read IsValid; override;
     method Seek(Offset: Int64; Origin: SeekOrigin): Int64; override;
-    method &Read(const buf: ^Void; Count: Int32): Int32; override;
-    method &Write(const buf: ^Void; Count: Int32): Int32; override;
+    method &Read(aSpan: Span<Byte>): Int32; override;
+    method &Write(aSpan: ImmutableSpan<Byte>): Int32; override;
     method ToArray: array of Byte;
     method WriteTo(Destination: Stream);
     {$IFNDEF NOFILES}
@@ -72,29 +72,27 @@ begin
   exit fPosition;
 end;
 
-method MemoryStream.Read(const buf: ^Void; Count: Int32): Int32;
+method MemoryStream.Read(aSpan: Span<Byte>): Int32;
 begin
   if not CanRead then raise new NotSupportedException;
-  if buf = nil then raise new Exception("argument is null");
-  if Count = 0 then exit 0;
+  if aSpan.Length = 0 then exit 0;
   var lres := fLength - fPosition;
   if lres <= 0 then exit 0;
-  if lres > Count then lres := Count;
-  memcpy(buf, @fbuf[fPosition], lres);
+  if lres > aSpan.Length then lres := aSpan.Length;
+  memcpy(aSpan.Pointer, @fbuf[fPosition], lres);
   fPosition := fPosition + lres;
   exit lres;
 end;
 
-method MemoryStream.Write(const buf: ^Void; Count: Int32): Int32;
+method MemoryStream.Write(aSpan: ImmutableSpan<Byte>): Int32;
 begin
   if not CanWrite then raise new NotSupportedException;
-  if buf = nil then raise new Exception("argument is null");
-  if Count = 0 then exit 0;
-  CheckCapacity(fPosition+Count);
-  memcpy(@fbuf[fPosition], buf, Count);
-  fPosition := fPosition+Count;
+  if aSpan.Length = 0 then exit 0;
+  CheckCapacity(fPosition + aSpan.Length);
+  memcpy(@fbuf[fPosition], aSpan.Pointer, aSpan.Length);
+  fPosition := fPosition + aSpan.Length;
   if fPosition > fLength then fLength := fPosition;
-  exit Count;
+  exit aSpan.Length;
 end;
 
 method MemoryStream.ToArray: array of Byte;
@@ -127,7 +125,7 @@ method MemoryStream.WriteTo(Destination: Stream);
 begin
   if Destination = nil then raise new Exception('Destination is null');
   if not Destination.CanWrite then raise new NotSupportedException;
-  Destination.Write(@fbuf[0],fLength);
+  Destination.Write(new Span<Byte>(fbuf, 0, fLength));
 end;
 
 {$IFNDEF NOFILES}
@@ -137,7 +135,7 @@ begin
   SetLength(0);
   self.Capacity := fs.Length;
   fs.Position := 0;
-  fLength := fs.Read(@fbuf[0], fs.Length);
+  fLength := fs.Read(new Span<Byte>(fbuf, 0, fLength));
   fs.Close;
 end;
 
@@ -146,7 +144,7 @@ begin
   var fs := new FileStream(FileName, FileMode.Create,FileAccess.Write, FileShare.None);
   SetLength(fLength);
   fs.Position := 0;
-  fs.Write(@fbuf[0],fLength);
+  fs.Write(new Span<Byte>(@fbuf[0], fLength));
   fs.Close;
 end;
 {$ENDIF}
---
+++ Source/Span.pas
@@ -0,0 +1,130 @@
+﻿namespace RemObjects.Elements.System;
+
+type
+  Span<T> = public record 
+  private
+    method set_Data(i: Integer; value: T);
+    begin 
+      if UInt32(i) <UInt32(fLength) then 
+        fPointer[i] := value 
+      else 
+        raise new IndexOutOfRangeException('i out of range');  
+    end;
+
+    method get_Data(i: Integer): T;
+    begin 
+      if UInt32(i) <UInt32(fLength) then 
+        exit fPointer[i] 
+      else 
+        raise new IndexOutOfRangeException('i out of range'); 
+    end;
+
+    fObject: Object;
+    fPointer: ^T;
+    fLength: Integer;
+  public
+    constructor; empty;
+
+    constructor(aArray: array of T; aStart, aLength: Integer);
+    begin
+      if aArray = nil then raise new ArgumentNullException('aArray');
+      if aLength < 0 then raise new ArgumentOutOfRangeException('aLength < 0');
+      if (aStart < 0) or (Cardinal(aStart + aLength) >= Cardinal(aArray.Length)) then new ArgumentOutOfRangeException('aStart < 0 or aStart + Length >= aArray.Length');
+      fObject := aArray;
+      fPointer := @aArray[aStart]; 
+      fLength := aLength;
+    end;
+
+    constructor(aPointer: ^T; aLength: Integer); 
+    begin 
+       fPointer := aPointer;
+       fLength := aLength;
+    end;
+
+    class property Empty: Span<T> read default(Span<T>);
+
+    property Empty: Boolean read fLength = 0;
+    property Length: Integer read fLength;
+    property Pointer: ^T read fPointer;
+    property Data[i: Integer]: T read get_Data write set_Data;
+
+    method Slice(aStart: Integer): Span<T>;
+    begin 
+      exit Slice(aStart, Length - aStart);
+    end;
+
+    method Slice(aStart, aLength: Integer): Span<T>; 
+    begin 
+      if (aStart < 0) or (aStart + aLength > Length) or (aLength < 0) then raise new ArgumentOutOfRangeException('Invalid start/length!');
+      exit new Span<T>(fObject := fObject, fLength := aLength, fPointer := @fPointer[aStart]);
+    end;
+
+    class operator Implicit(aVal: array of T): Span<T>;
+    begin 
+      exit new Span<T>(aVal, 0, aVal.Length);
+    end;
+
+    class operator Implicit(aVal: Span<T>): ReadOnlySpan<T>;
+    begin 
+      exit new ReadOnlySpan<T>(fObject := aVal.fObject, fPointer := aVal.fPointer, fLength := aVal.fLength);
+    end;
+  end;
+
+  ReadOnlySpan<T> = public ImmutableSpan<T>;
+  ImmutableSpan<T> = public record 
+  assembly
+    method get_Data(i: Integer): T;
+    begin 
+      if UInt32(i) <UInt32(fLength) then 
+        exit fPointer[i] 
+      else 
+        raise new IndexOutOfRangeException('i out of range'); 
+    end;
+
+    fObject: Object;
+    fPointer: ^T;
+    fLength: Integer;
+  public
+    constructor; empty;
+
+    constructor(aArray: array of T; aStart, aLength: Integer);
+    begin
+      if aArray = nil then raise new ArgumentNullException('aArray');
+      if aLength < 0 then raise new ArgumentOutOfRangeException('aLength < 0');
+      if (aStart < 0) or (Cardinal(aStart + aLength) >= Cardinal(aArray.Length)) then new ArgumentOutOfRangeException('aStart < 0 or aStart + Length >= aArray.Length');
+      fObject := aArray;
+      fPointer := @aArray[aStart]; 
+      fLength := aLength;
+    end;
+
+    constructor(aPointer: ^T; aLength: Integer); 
+    begin 
+       fPointer := aPointer;
+       fLength := aLength;
+    end;
+
+    class property Empty: Span<T> read default(Span<T>);
+
+    property Empty: Boolean read fLength = 0;
+    property Length: Integer read fLength;
+    property Pointer: ^T read fPointer;
+    property Data[i: Integer]: T read get_Data;
+
+    method Slice(aStart: Integer): ImmutableSpan<T>;
+    begin 
+      exit Slice(aStart, Length - aStart);
+    end;
+
+    method Slice(aStart, aLength: Integer): ImmutableSpan<T>; 
+    begin 
+      if (aStart < 0) or (aStart + aLength > Length) or (aLength < 0) then raise new ArgumentOutOfRangeException('Invalid start/length!');
+      exit new ImmutableSpan<T>(fObject := fObject, fLength := aLength, fPointer := @fPointer[aStart]);
+    end;
+
+    class operator Implicit(aVal: array of T): ImmutableSpan<T>;
+    begin 
+      exit new ImmutableSpan<T>(aVal, 0, aVal.Length);
+    end;
+  end;
+
+end.
