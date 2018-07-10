4c3e46cdd1789fe27f551451aa3b67363d9a77ff
evgeny-k <git@evgenysoft.com>
2018-07-10 13:27:02 +0300

    80465: Parallel For and ForEach class for Island, Toffee and Cooper

---
+++ Source/Parallel.pas
@@ -0,0 +1,59 @@
+﻿namespace RemObjects.Elements.System;
+{$IFNDEF NOTHREADS}
+type
+  ParallelLoopState = public class
+  private
+  public
+    property IsStopped: Boolean read private write;
+    method &Break;
+    begin
+      IsStopped := True;
+    end;
+  end;
+
+
+  &Parallel = static public class
+  public
+    class method &For(fromInclusive: Integer; toExclusive: Integer; body: Action<Integer,ParallelLoopState>);
+    begin
+      var list := new List<Integer>;
+      for i: Integer := toExclusive to fromInclusive-1 do
+        list.Add(i);
+      ForEach<Integer>(list,body);      
+    end;
+
+
+    class method &For(fromInclusive: Int64; toExclusive: Int64; body: Action<Int64,ParallelLoopState>);
+    begin
+      var list := new List<Int64>;
+      for i: Int64 := toExclusive to fromInclusive-1 do
+        list.Add(i);
+      ForEach<Int64>(list,body);      
+    end;
+
+    class method ForEach<TSource>(source: IEnumerable<TSource>; body: Action<TSource,ParallelLoopState>);
+    begin
+      var lthreadcnt := Environment.ProcessorCount;
+      var lcurrTasks: Integer := 0;
+      var levent := new EventWaitHandle(true, false);
+      var ls:= new ParallelLoopState();
+      for m in source do begin
+        while InternalCalls.Add(var lcurrTasks, 0) >= lthreadcnt do begin
+          if ls.IsStopped then Break;
+          levent.Wait;
+        end;
+        if ls.IsStopped then Break;
+        InternalCalls.Add(var lcurrTasks, 1);
+        new Task(->
+          begin
+            body(m, ls);
+            InternalCalls.Add(var lcurrTasks, -1);
+            levent.Set;
+          end).Start;
+      end;
+      while InternalCalls.Add(var lcurrTasks, 0) > 0 do levent.Wait;
+    end;
+  end;
+
+{$ENDIF}
+end.
