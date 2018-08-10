b98d66d22697e9345ee0b800b251c087bca85b60
Carlo Kok <ck@remobjects.com>
2018-08-10 11:48:46 +0200

    0: Preparative work for supporting objc classes in Island/Darwin.

---
+++ Source/CocoaGC.pas
@@ -0,0 +1,171 @@
+﻿namespace RemObjects.Elements.System;
+type
+  ObjcStrong<T> = public lifetimestrategy (ObjcStrong) T;
+  ObjcStrong = public record(ILifetimeStrategy<ObjcStrong>)
+  assembly
+    fInst: IntPtr;
+  public
+    [SymbolName('objc_retain')]
+    class method objc_retain(aVal: IntPtr): IntPtr; external;
+
+    [SymbolName('objc_release')]
+    class method objc_release(aVal: IntPtr); external;
+
+    [SymbolName('objc_storeStrong')]
+    class method objc_storeStrong(var aDest: IntPtr; aSrc: IntPtr); external;
+
+    [SymbolName('objc_retainAutoreleaseReturnValue')]
+    class method objc_retainAutoreleaseReturnValue(aVal: IntPtr): IntPtr; external;
+
+    [SymbolName('objc_retainAutoreleasedReturnValue')]
+    class method objc_retainAutoreleasedReturnValue(aVal: IntPtr): IntPtr; external;
+
+    class method &New(aTTY: ^Void; aSize: NativeInt): ^Void;
+    begin
+      raise new Exception('Cocoa types should be allocated through alloc');
+    end;
+
+
+    constructor Copy(var aValue: ObjcWeak);
+    begin
+      fInst := ObjcWeak.objc_loadWeakRetained(aValue.fInst);
+    end;
+
+    class method Init(var Dest: ObjcStrong);
+    begin
+      Dest.fInst := 0;
+    end;
+
+    constructor Copy(var aValue: ObjcStrong);
+    begin
+      fInst := objc_retain(aValue.fInst);
+    end;
+
+    class method Copy(var aDest, aSource: ObjcStrong);
+    begin
+      aDest.fInst := objc_retain(aSource.fInst);
+    end;
+
+    class operator Assign(var aDest: ObjcStrong; var aSource: ObjcStrong);
+    begin
+      Assign(var aDest, var aSource);
+    end;
+
+    class method Assign(var aDest, aSource: ObjcStrong);
+    begin
+      objc_storeStrong(var aDest.fInst, aSource.fInst);
+    end;
+
+    class method Assign(var aDest: ObjcStrong; var aSource: ObjcWeak);
+    begin
+      var lTmp := ObjcWeak.objc_loadWeakRetained(aSource.fInst);
+      objc_storeStrong(var aDest.fInst, lTmp);
+      objc_release(lTmp);
+    end;
+
+    class method Release(var aDest: ObjcStrong);
+    begin
+      objc_storeStrong(var aDest.fInst, 0);
+    end;
+
+    class method ReturnValue(aValue: ObjcStrong): ObjcStrong;
+    begin
+      result.fInst := objc_retainAutoreleaseReturnValue(aValue.fInst);
+    end;
+
+    class method GetReturnValue(aValue: ObjcStrong): ObjcStrong;
+    begin
+      result.fInst := objc_retainAutoreleasedReturnValue(aValue.fInst);
+    end;
+
+    finalizer;
+    begin
+      Release(var self);
+    end;
+  end;
+
+
+  ObjcWeak<T> = public lifetimestrategy (ObjcWeak) T;
+  ObjcWeak = public record(ILifetimeStrategy<ObjcWeak>)
+  assembly
+    fInst: IntPtr;
+  public
+    [SymbolName('objc_initWeak')]
+    class method objc_initWeak(var aVal: IntPtr; aIntVal: IntPtr); external;
+    [SymbolName('objc_destroyWeak')]
+    class method objc_destroyWeak(var aVal: IntPtr); external;
+
+    [SymbolName('objc_copyWeak')]
+    class method objc_copyWeak(var aDest: IntPtr; var aSrc: IntPtr); external;
+
+    [SymbolName('objc_storeWeak')]
+    class method objc_storeWeak(var aDest: IntPtr; aSrc: IntPtr); external;
+
+    [SymbolName('objc_loadWeakRetained')]
+    class method objc_loadWeakRetained(aVal: IntPtr): IntPtr; external;
+
+    [SymbolName('objc_retainAutoreleasedReturnValue')]
+    class method objc_retainAutoreleasedReturnValue(aVal: IntPtr): IntPtr; external;
+
+    class method &New(aTTY: ^Void; aSize: NativeInt): ^Void;
+    begin
+      raise new Exception('Cocoa types should be allocated through alloc');
+    end;
+
+    class method Init(var Dest: ObjcWeak);
+    begin
+      objc_initWeak(var Dest.fInst, nil);
+    end;
+
+    constructor Copy(var aValue: ObjcWeak);
+    begin
+      var lTmp := objc_loadWeakRetained(aValue.fInst);
+      objc_initWeak(var fInst, lTmp);
+      ObjcStrong.objc_release(lTmp);
+    end;
+
+    constructor Copy(var aValue: ObjcStrong);
+    begin
+      objc_initWeak(var fInst, aValue.fInst);
+    end;
+
+    class method Copy(var aDest, aSource: ObjcWeak);
+    begin
+      var lTmp := objc_loadWeakRetained(aSource.fInst);
+      objc_initWeak(var aDest.fInst, lTmp);
+      ObjcStrong.objc_release(lTmp);
+    end;
+
+    class operator Assign(var aDest: ObjcWeak; var aSource: ObjcWeak);
+    begin
+      Assign(var aDest, var aSource);
+    end;
+
+    class operator Assign(var aDest: ObjcWeak; var aSource: ObjcStrong);
+    begin
+      Assign(var aDest, var aSource);
+    end;
+
+    class method Assign(var aDest, aSource: ObjcWeak);
+    begin
+      var lTmp := objc_loadWeakRetained(aSource.fInst);
+      objc_storeWeak(var aDest.fInst, lTmp);
+      ObjcStrong.objc_release(lTmp);
+    end;
+
+    class method Assign(var aDest: ObjcWeak; var aSource: ObjcStrong);
+    begin
+      objc_storeWeak(var aDest.fInst, aSource.fInst);
+    end;
+
+    class method Release(var aDest: ObjcWeak);
+    begin
+      objc_destroyWeak(var aDest.fInst);
+    end;
+
+    finalizer;
+    begin
+      Release(var self);
+    end;
+  end;
+end.
