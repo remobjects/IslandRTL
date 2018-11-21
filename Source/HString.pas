b18e7d5555cfd6c783ffa78168b647d91e618740
Carlo Kok <ck@remobjects.com>
2018-11-21 13:31:27 +0100

    81465: Remove hardcoded Winrt HString dependency

--- Source/HString.pas
+++ Source/HString.pas
@@ -3,59 +3,81 @@
 [Assembly: RemObjects.Elements.System.LifetimeStrategyOverrideAttribute(typeOf(rtl.winrt.HSTRING), typeOf(HString_Helper))]
 
 type
+  [CallingConvention(CallingConvention.Stdcall)]
+  DeleteStringSig = function(str: rtl.winrt.HSTRING): rtl.HRESULT;
+  [CallingConvention(CallingConvention.Stdcall)]
+  CreateStringSig = function(source: ^Char; len: UInt32; str: ^rtl.winrt.HSTRING): rtl.HRESULT;
+  [CallingConvention(CallingConvention.Stdcall)]
+  DuplicateStringSig = function(str: rtl.winrt.HSTRING; str2: ^rtl.winrt.HSTRING): rtl.HRESULT;
+  [CallingConvention(CallingConvention.Stdcall)]
+  GetStringBufferStringSig = function(str: rtl.winrt.HSTRING; len: ^UInt32): ^Char;
   HString_Helper = public record
-  private
+  assembly
     fStr: rtl.winrt.HSTRING;
+    class var fDeleteString: DeleteStringSig;
+    class var fCreateString: CreateStringSig;
+    class var fDuplicatestring: DuplicateStringSig;
+    class var fGetStringBuffer: GetStringBufferStringSig;
   protected
   public
-    constructor;
-    begin 
-      fStr := nil;
-    end;
-    
-    constructor Copy(var aValue: HString_Helper);
-    begin 
-      if aValue.fStr = nil then 
-        fStr := nil 
-      else 
-        rtl.winrt.WindowsDuplicateString(aValue.fStr, @fStr);
+    class constructor;
+    begin
+      var lLibrary := rtl.LoadLibrary('api-ms-win-core-winrt-string-l1-1-0.dll');
+      if lLibrary = nil then raise new Exception('HString requires Windows 8+');
+      fDeleteString := DeleteStringSig(rtl.GetProcAddress(lLibrary, 'WindowsDeleteString'));
+      fCreateString := CreateStringSig(rtl.GetProcAddress(lLibrary, 'WindowsCreateString'));
+      fDuplicatestring := DuplicateStringSig(rtl.GetProcAddress(lLibrary, 'WindowsDuplicateString'));
+      fGetStringBuffer := GetStringBufferStringSig(rtl.GetProcAddress(lLibrary, 'WindowsGetStringRawBuffer'));
     end;
 
-    
+    constructor;
+    begin
+      fStr := nil;
+    end;
+
+    constructor Copy(var aValue: HString_Helper);
+    begin
+      if aValue.fStr = nil then
+        fStr := nil
+      else
+        fDuplicatestring(aValue.fStr, @fStr);
+    end;
+
+
     class operator Assign(var aDest: HString_Helper; var aSource: HString_Helper);
     begin
       if (@aDest) = (@aSource) then exit;
-      if aDest.fStr <> nil then 
-        rtl.winrt.WindowsDeleteString(aDest.fStr);
-      if aSource.fStr = nil then begin 
+      if aDest.fStr <> nil then
+        fDeleteString(aDest.fStr);
+      if aSource.fStr = nil then begin
         aDest.fStr := nil;
-      end else 
-        rtl.winrt.WindowsDuplicateString(aDest.fStr, @aSource.fStr);
+      end else
+        fDuplicatestring(aDest.fStr, @aSource.fStr);
     end;
-    
+
     finalizer;
-    begin 
-      if fStr <> nil then 
-        rtl.winrt.WindowsDeleteString(fStr);
+    begin
+      if fStr <> nil then
+        fDeleteString(fStr);
       fStr := nil;
     end;
   end;
-  
+
 operator Implicit(s: rtl.winrt.HSTRING): String; public;
 begin
   if s = nil then exit nil;
   var len: UInt32;
-  exit String.FromPChar(rtl.winrt.WindowsGetStringRawBuffer(s, @len), len);
+  exit String.FromPChar(HString_Helper.fGetStringBuffer(s, @len), len);
 end;
 
 operator Implicit(s: String): rtl.winrt.HSTRING; public;
 begin
-  rtl.winrt.WindowsCreateString(@s.fFirstChar, s.Length, @result);
+  HString_Helper.fCreatestring(@s.fFirstChar, s.Length, @result);
 end;
 
 operator Implicit(s: ^rtl.WCHAR): rtl.winrt.HSTRING; public;
 begin
-  rtl.winrt.WindowsCreateString(s, ExternalCalls.wcslen(s), @result);
+  HString_Helper.fCreatestring(s, ExternalCalls.wcslen(s), @result);
 end;
-  
+
 end.
