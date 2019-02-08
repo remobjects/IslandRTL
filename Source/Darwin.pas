56c9e0f3cd3f7fc01cdf43cbebf8437a4efebce4
Carlo Kok <ck@remobjects.com>
2019-02-08 11:23:07 +0100

    0: Missing stack/global block dllimport.

--- Source/Darwin.pas
+++ Source/Darwin.pas
@@ -30,6 +30,8 @@ uses
 [assembly:DllImport('/usr/lib/libobjc.A.dylib', EntryPoint := '_objc_autoreleasePoolPop')]
 [assembly:DllImport('/usr/lib/libobjc.A.dylib', EntryPoint := '_objc_ehtype_vtable')]
 [assembly:DllImport('/usr/lib/system/libdyld.dylib', EntryPoint := '__tlv_bootstrap')]
+[assembly:DllImport('/usr/lib/libobjc.A.dylib', EntryPoint := '_NSConcreteGlobalBlock')]
+[assembly:DllImport('/usr/lib/libobjc.A.dylib', EntryPoint := '_NSConcreteStackBlock')]
 
 [assembly:DllImport('/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation', EntryPoint := '___CFConstantStringClassReference')]
 
