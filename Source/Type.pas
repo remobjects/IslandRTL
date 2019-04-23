85ebb9dc21c200c57436d9f468c5810d0fb8b1eb
Carlo Kok <ck@remobjects.com>
2019-04-23 11:58:18 +0200

    82431: Darwin: .dylib fails to link

--- Source/Type.pas
+++ Source/Type.pas
@@ -645,7 +645,7 @@ type
        exit fAllTypes;
      end;
     {$ELSEIF DARWIN}
-    [&Weak, SymbolName('__mh_elements_execute_header')]
+    [&Weak, SymbolName('_mh_elements_execute_header')]
     class var __mh_elements_execute_header: {$IFDEF CPU64}rtl.__struct_mach_header_64{$ELSE}rtl.__struct_mach_header{$ENDIF} ; external;
 
     class method GetHDR: ^{$IFDEF CPU64}rtl.__struct_mach_header_64{$ELSE}rtl.__struct_mach_header{$ENDIF};
