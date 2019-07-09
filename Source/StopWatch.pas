6c569c4ec8f5494f4a14cd0c058096d6c9962245
Carlo Kok <ck@remobjects.com>
2019-07-09 11:39:29 +0200

    0: contributed class StopWatch by Paul Berger.

---
+++ Source/StopWatch.pas
@@ -0,0 +1,60 @@
+﻿namespace Stopwatch;
+
+interface
+
+type
+  StopWatch = public class
+  private
+    fStartTicks : Int64;
+    fEndTicks : Int64;
+    fIsRunning : Boolean;
+    method getElapsed( Milliseconds: Boolean) : Int64;
+  protected
+  public
+    constructor;
+    method Start;
+    method Stop;
+    method Reset;
+    property ElapsedTicks: Int64 read getElapsed( false );
+    property ElapsedMilliseconds: Int64 read getElapsed( true );
+    property IsRunning: Boolean read fIsRunning;
+  end;
+
+implementation
+
+constructor StopWatch;
+begin
+end;
+
+method StopWatch.Start;
+begin
+  fIsRunning := true;
+  fEndTicks := 0;
+  fStartTicks := DateTime.UtcNow.Ticks;
+end;
+
+method StopWatch.Stop;
+begin
+  fEndTicks := DateTime.UtcNow.Ticks;
+  fIsRunning := false;
+end;
+
+method StopWatch.Reset;
+begin
+  fStartTicks := 0;
+  fEndTicks := 0;
+  fIsRunning := false;
+end;
+
+method StopWatch.getElapsed(Milliseconds : Boolean): Int64;
+begin
+  if fIsRunning then
+    result := DateTime.UtcNow.Ticks - fStartTicks
+  else
+    result := fEndTicks - fStartTicks;
+
+  if Milliseconds and (result > 0) then
+    result := result / 1000;
+end;
+
+end.
