b42634ab54616fd597ac0ca6029ae7f306687b06
Diego Navarro <diegon@remobjects.com>
2019-07-16 19:51:28 +0200

    Fix FileStream.IsValid for Posix: comparing with nil generates wrong result when fHandle = 0 (standard output handle)

--- Source/FileStream.pas
+++ Source/FileStream.pas
@@ -111,7 +111,7 @@ begin
   {$IFDEF WINDOWS}
   exit fHandle <> rtl.INVALID_HANDLE_VALUE;
   {$ELSEIF POSIX}
-  exit fHandle <> nil;
+  exit fHandle <> PlatformHandle(-1);
   {$ELSE}
     {$ERROR}
   {$ENDIF}
