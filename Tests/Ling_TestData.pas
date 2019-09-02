16ba23bb1a85141c716b7be4bb41036b79405da7
Westermann <fwestermann@me.com>
2019-09-02 14:18:57 +0200

    Added some Tests for Linq-Support (WIP)

---
+++ Tests/Ling_TestData.pas
@@ -0,0 +1,75 @@
+﻿namespace RemObjects.Elements.Island.Tests;
+type
+  TUser = public class
+  public
+    property ID : Integer;
+    property Name : String;
+    property Age : Integer;
+    property HomeCountry : String;
+    property Company : Integer;
+
+    constructor (aID : Integer; aName : String; aAge : Integer; aHomeCountry : String; aCompany : Integer);
+    begin
+      ID := aID;
+      Name := aName;
+      Age := aAge;
+      HomeCountry := aHomeCountry;
+      Company := aCompany;
+    end;
+
+    [ToString]
+    method ToString : String;
+    begin
+      exit $"ID: {ID}, Name: {Name}, Age: {Age}, Country : {HomeCountry}, CompanyId: {Company}";
+    end;
+
+  end;
+
+  TCompany = public class
+  public
+  property ID : Integer;
+  property Name : String;
+  property HomeCountry : String;
+
+
+
+  constructor (aID : Integer; aName : String; aHomeCountry : String);
+  begin
+    Name := aName;
+    ID := aID;
+    HomeCountry := aHomeCountry;
+  end;
+end;
+
+
+  Test_Data =  static class
+  public
+   method PrepareUsers : List<TUser>;
+    begin
+      exit new List<TUser>([
+      new TUser(1,'John Doe', 42, 'USA',1),
+      new TUser(2,'Jane Doe', 38, 'USA',2),
+      new TUser(3,'Joe Doe', 20, 'Germany',3),
+      new TUser(4,'Jenna Doe', 19, 'Germany',4),
+      new TUser(5,'Marc Hofmann', 32, 'Curacao',1),
+      new TUser(6,'James Doe', 8, 'USA',6)
+      ]);
+
+    end;
+
+    method PrepareCompanies : List<TCompany>;
+    begin
+      exit new List<TCompany>([
+      new TCompany(1,'Remobjects', 'USA'),
+      new TCompany(2,'Apple', 'USA'),
+      new TCompany(3,'Microsoft', 'USA'),
+      new TCompany(4,'Huawei', 'China'),
+      new TCompany(5,'Nixdorf', 'Germany'),
+      new TCompany(6,'SAP', 'Germany')
+      ]);
+
+    end;
+
+  end;
+
+end.
---
+++ Tests/Ling_Tests.pas
@@ -0,0 +1,115 @@
+﻿namespace RemObjects.Elements.Island.Tests;
+{$DEFINE COMPILEBREAK}
+interface
+
+uses
+  RemObjects.Elements.EUnit;
+
+type
+  Linq_Tests = public class(Test)
+  private
+
+   public
+    method SingleWhere;
+    method MultiWhere;
+    method TestOrderBy;
+    method TestOrderByDesc;
+
+    method TestOrderByMulti;
+    method TestJoin;
+  end;
+
+
+implementation
+
+method Linq_Tests.SingleWhere;
+begin
+  var Users :=
+     from User in Test_Data.PrepareUsers
+     Where User.Age ≥ 20;
+   Assert.AreEqual(Users.Count, 4);
+
+end;
+
+method Linq_Tests.MultiWhere;
+begin
+  var Users :=
+     from User in Test_Data.PrepareUsers
+     Where (User.Age ≥ 20) and (User.HomeCountry = 'USA');
+   Assert.AreEqual(Users.Count, 2);
+   Var UserArray := Users.ToArray;
+  Assert.AreEqual(UserArray[0].ID, 1);
+  Assert.AreEqual(UserArray[1].ID, 2);
+end;
+
+method Linq_Tests.TestOrderBy;
+begin
+  var Users :=
+       from User in Test_Data.PrepareUsers
+        Order by User.Age;
+  for each User in Users index i do
+   begin
+    case i  of
+      0 : Assert.AreEqual(User.Age, 8, $'Wrong Age in {i} is {User.Age}');
+      1 : Assert.AreEqual(User.Age, 19, $'Wrong Age in {i} is {User.Age}');
+      2 : Assert.AreEqual(User.Age, 20, $'Wrong Age in {i} is {User.Age}');
+      3 : Assert.AreEqual(User.Age, 32, $'Wrong Age in {i} is {User.Age}');
+    end;
+   end;
+
+end;
+
+method Linq_Tests.TestOrderByDesc;
+begin
+  var Users :=
+       from User in Test_Data.PrepareUsers
+        Order by User.Age desc;
+  for each User in Users index i do
+    begin
+    case i  of
+      0 : Assert.AreEqual(User.Age, 42, $'Wrong Age in {i} is {User.Age}');
+      1 : Assert.AreEqual(User.Age, 38, $'Wrong Age in {i} is {User.Age}');
+      2 : Assert.AreEqual(User.Age, 32, $'Wrong Age in {i} is {User.Age}');
+      3 : Assert.AreEqual(User.Age, 20, $'Wrong Age in {i} is {User.Age}');
+    end;
+  end;
+
+end;
+
+method Linq_Tests.TestOrderByMulti;
+begin
+  var Users :=
+       from User in Test_Data.PrepareUsers
+        Order by User.HomeCountry,  User.Age;
+
+  for each User in Users index i do
+    begin
+    case i  of
+      0 : Assert.AreEqual(User.Age, 32, $'Wrong Age in {i} is {User.ToString}');
+      1 : Assert.AreEqual(User.Age, 19, $'Wrong Age in {i} is {User.ToString}');
+      2 : Assert.AreEqual(User.Age, 20, $'Wrong Age in {i} is {User.ToString}');
+      3 : Assert.AreEqual(User.Age, 8, $'Wrong Age in {i} is {User.ToString}');
+    end;
+  end;
+
+end;
+
+method Linq_Tests.TestJoin;
+begin
+ {$IFDEF COMPILEBREAK}
+  var lJoinList :=
+      from User in Test_Data.PrepareUsers
+      order by User.HomeCountry
+      join Company in Test_Data.PrepareCompanies on User.Company equals Company.ID
+      select new class (CompanyName := Company.Name , Employe := User)// into P
+    //  where P.CompanyName = 'USA'
+    ;
+
+  for each entry in lJoinList index i do begin
+    case i  of
+      0 : Assert.AreEqual(entry.CompanyName, 'Remobjects');
+    end;
+   end;
+ {$ENDIF}
+end;
+end.
