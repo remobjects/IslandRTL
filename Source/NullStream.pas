54e9a46d17fb4d61b1247981b37a0534df8f1f15
marc hoffman <mh@remobjects.com>
2019-09-23 12:20:10 -0400

    NullStream; case auto-fix

---
+++ Source/NullStream.pas
@@ -0,0 +1,37 @@
+﻿namespace RemObjects.Elements.System;
+
+type
+  NulStream = public class(Stream)
+  protected
+
+    method IsValid: Boolean; override;
+    begin
+      result := true;
+    end;
+
+  public
+    constructor; empty;
+    class property Instance: NulStream := new NulStream; lazy; readonly;
+
+    property CanRead: Boolean read true; override;
+    property CanSeek: Boolean read true; override;
+    property CanWrite: Boolean read true; override;
+    method Seek(Offset: Int64; Origin: SeekOrigin): Int64; override; empty;
+
+    method &Read(aSpan: Span<Byte>): Int32; override;
+    begin
+      result := 0;
+    end;
+
+    method &Write(aSpan: ImmutableSpan<Byte>): Int32; override; empty;
+
+    method ToArray: array of Byte;
+    begin
+      result := [];
+    end;
+
+    property Length: Int64 read 0; override;
+    method SetLength(value: Int64); override; empty;
+  end;
+
+end.
