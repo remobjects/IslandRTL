ee4589fa58c88ef91e62cd805a41ed75ab9b9441
Carlo Kok <ck@remobjects.com>
2019-10-24 12:08:22 +0200

    0: Dynamically load Swift to avoid the dependency.

--- Source/Swift.pas
+++ Source/Swift.pas
@@ -4,40 +4,45 @@ type
   SwiftStrong<T> = public lifetimestrategy (SwiftStrong) T;
   OnceCallback = public procedure(ctx: ^Void);
   SwiftInt = public IntPtr;
+  SwiftIntPtrApi = public function(i: IntPtr): IntPtr;
+  SwiftAllocApi = public function(aMD: ^Void; aRequiredSize, aRequiredAlignmentMask: IntPtr): ^Void;
+  SwiftInitApi = public function(aMD: ^Void; aRequiredSize, aRequiredAlignmentMask: IntPtr): ^Void;
+  SwiftBeginAccessApi = public procedure(pointer: ^Void; buffer: ^Void; flags: UIntPtr; pc: ^Void);
+
   SwiftStrong = public record(ILifetimeStrategy<SwiftStrong>)
   assembly
     fInst: IntPtr;
   public
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_allocObject')]
-    class method swift_allocObject(aMD: ^Void; aRequiredSize, aRequiredAlignmentMask: IntPtr): ^Void;external;
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_initStackObject')]
-    class method swift_initStackObject(aMD: ^Void; aTarget: ^Void): ^Void;external;
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_initStaticObject')]
-    class method swift_initStaticObject(aMD: ^Void; aTarget: ^Void): ^Void;external;
+    class constructor;
+    begin
+      var lDLL := rtl.dlopen('libswiftCore.dylib', 0);
+      if lDLL = nil then raise new Exception('libswiftCore.dylib not present!');
 
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_beginAccess')]
-    class method swift_beginAccess(pointer: ^Void; buffer: ^Void; flags: UIntPtr; pc: ^Void);external;
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_endAccess')]
-    class method swift_endAccess(buffer: ^Void);external;
+      swift_getInitializedObjCClass := SwiftIntPtrApi(rtl.dlsym(lDLL, 'swift_getInitializedObjCClass'));
+      swift_endAccess := SwiftIntPtrApi(rtl.dlsym(lDLL, 'swift_endAccess'));
+      swift_bridgeObjectRetain := SwiftIntPtrApi(rtl.dlsym(lDLL, 'swift_bridgeObjectRetain'));
+      swift_bridgeObjectRelease :=SwiftIntPtrApi( rtl.dlsym(lDLL, 'swift_bridgeObjectRelease'));
+      swift_retain := SwiftIntPtrApi(rtl.dlsym(lDLL, 'swift_retain'));
+      swift_release := SwiftIntPtrApi(rtl.dlsym(lDLL, 'swift_release'));
+      swift_allocObject := SwiftAllocApi(rtl.dlsym(lDLL, 'swift_allocObject'));
+      swift_deallocClassInstance := SwiftAllocApi(rtl.dlsym(lDLL, 'swift_deallocClassInstance'));
+      swift_initStackObject := SwiftInitApi(rtl.dlsym(lDLL, 'swift_initStackObject'));
+      swift_initStaticObject := SwiftInitApi(rtl.dlsym(lDLL, 'swift_initStaticObject'));
+      swift_beginAccess := SwiftBeginAccessApi(rtl.dlsym(lDLL, 'swift_beginAccess'));
+    end;
 
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_once')]
-    class method swift_once(once: ^Integer; cb: OnceCallback; ctx: ^Void);external;
+    class var swift_getInitializedObjCClass: SwiftIntPtrApi;
+    class var swift_endAccess: SwiftIntPtrApi;
+    class var swift_bridgeObjectRetain: SwiftIntPtrApi;
+    class var swift_bridgeObjectRelease: SwiftIntPtrApi;
+    class var swift_retain: SwiftIntPtrApi;
+    class var swift_release: SwiftIntPtrApi;
+    class var swift_allocObject: SwiftAllocApi;
+    class var swift_deallocClassInstance: SwiftAllocApi;
+    class var swift_initStackObject: SwiftInitApi;
+    class var swift_initStaticObject: SwiftInitApi;
+    class var swift_beginAccess: SwiftBeginAccessApi;
 
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_bridgeObjectRetain')]
-    class method swift_bridgeObjectRetain(val: ^Void): ^Void; external;
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_bridgeObjectRelease')]
-    class method swift_bridgeObjectRelease(val: ^Void); external;
-
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_retain')]
-    class method swift_retain(val: IntPtr): IntPtr; external;
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_release')]
-    class method swift_release(val: IntPtr); external;
-
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_deallocClassInstance')]
-    class method swift_deallocClassInstance(val: ^Void; aAllocatedSize, aAllocatedAlignMask: IntPtr); external;
-
-    [DllImport('libswiftCore.dylib', EntryPoint := 'swift_getInitializedObjCClass')]
-    class method swift_getInitializedObjCClass(c: rtl.Class): rtl.Class; external;
 
     class method &New(aTTY: ^Void; aSize: NativeInt): ^Void;
     begin
