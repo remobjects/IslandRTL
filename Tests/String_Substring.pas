dd69671555b55e080264d8284984fe753a4badb3
Westermann <fwestermann@me.com>
2020-02-26 09:00:44 +0100

    Change behavior of method String.Substring(StartIndex: Integer; aLength: Integer): not nullable String;
    If aLength = 0 it should return a Empty String to match NET and Delphi

---
+++ Tests/String_Substring.pas
@@ -0,0 +1,36 @@
+﻿namespace Elements.RTL2.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit;
+
+type
+  String_SubString = public class(Test)
+  public
+
+    method Substring;
+    begin
+      var sStart := '012345678901234';
+      var l := sStart.Length;
+      Assert.AreEqual(l, 15);
+      // First Char
+      var s := sStart.Substring(0,1);
+      Assert.AreEqual(s, '0');
+
+      // Last Char
+      s := sStart.Substring(14,1);
+      Assert.AreEqual(s, '4');
+
+     //count 0 should be empty Char
+      s := sStart.Substring(14,0);
+      Assert.AreEqual(s, '');
+
+    //count 0 should be empty Char and not crash
+    // Net and Delphi will simply resturn a empty String
+      s := sStart.Substring(15,0);
+      Assert.AreEqual(s, '');
+
+    end;
+
+  end;
+
+end.
