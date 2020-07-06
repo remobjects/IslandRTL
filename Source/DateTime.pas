70d1de00c9dfc75dbfea2cbf28099705f2676557
Diego Navarro <diegon@remobjects.com>
2020-07-06 16:28:30 +0200

    84595: Islands vs Toffee: TimeSince platform differences in Island RTL

--- Source/DateTime.pas
+++ Source/DateTime.pas
@@ -101,11 +101,11 @@ type
 			{$ELSEIF ANDROID or DARWIN}
 			var ts: rtl.__struct_timespec;
 			rtl.clock_gettime({$IFDEF DARWIN}rtl.clockid_t._CLOCK_REALTIME{$ELSE}rtl.CLOCK_REALTIME{$ENDIF}, @ts);
-			exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerMillisecond) + (ts.tv_nsec / 100000));
+			exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerSecond) + (ts.tv_nsec / 100000));
 			{$ELSEIF POSIX}
 			var ts: rtl.__struct_timespec;
 			rtl.timespec_get(@ts, rtl.TIME_UTC);
-			exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerMillisecond) + (ts.tv_nsec / 100000));
+			exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerSecond) + (ts.tv_nsec / 100000));
 			{$ELSEIF WEBASSEMBLY}
 			exit new DateTime(Int64(UnixDateOffset + (WebAssemblyCalls.GetUTCTime * TicksPerMillisecond)));
 			{$ELSE}{$ERROR}
@@ -123,7 +123,8 @@ type
 			{$ELSEIF ANDROID or DARWIN}
 			var ts: rtl.__struct_timespec;
 			rtl.clock_gettime({$IFDEF DARWIN}rtl.clockid_t._CLOCK_REALTIME{$ELSE}rtl.CLOCK_REALTIME{$ENDIF}, @ts);
-			exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerMillisecond) + (ts.tv_nsec / 100000));
+			//exit new DateTime(UnixDateOffset + (ts.tv_sec * TicksPerSecond) + (ts.tv_nsec / 100000));
+			exit FromUnixTimeUTC(ts);
 			{$ELSEIF POSIX}
 			var ts: rtl.__struct_timespec;
 			rtl.timespec_get(@ts, rtl.TIME_UTC);
