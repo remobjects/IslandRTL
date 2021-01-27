7e60d873cbccc1aa155aa2261b7bfa450ada03e7
Carlo Kok <ck@remobjects.com>
2021-01-27 14:38:14 +0100

    0: Memory type for Island

---
+++ Source/Memory.cs
@@ -0,0 +1,32 @@
+﻿namespace RemObjects.Elements.System
+{
+	public struct Memory<T> {
+		private Object inst;
+		private IntPtr offset;
+
+		public Memory(Object inst, IntPtr offset)
+		{
+			this.inst = inst;
+			this.offset = offset;
+		}
+
+		public unsafe ref T Ref() {
+			if (inst == null) {
+				return ref (*((T*)offset));
+			}
+
+			ref T res = (T*)(*((IntPtr*)(&inst)) + offset);
+			return ref res;
+		}
+		
+		public T Value {
+		  get {
+		    return Ref();
+		  }
+		  set {
+		    var x = ref Ref();
+		    x = value;
+		  }
+		}
+	}
+}
