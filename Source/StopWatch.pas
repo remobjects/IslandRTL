a5dc66e5166d9a108299440ae8ca485ceeaff058
wxinix <xinwuping007@gmail.com>
2021-04-17 16:26:30 -0400

    Fixed bug in StopWatch.GetElapsed. The previous code converts ticks to milliseconds using the wrong factor.

--- Source/StopWatch.pas
+++ Source/StopWatch.pas
@@ -5,18 +5,18 @@ interface
 type
   StopWatch = public class
   private
-    fStartTicks : Int64;
-    fEndTicks : Int64;
-    fIsRunning : Boolean;
-    method getElapsed( Milliseconds: Boolean) : Int64;
+    fStartTicks: Int64;
+    fEndTicks: Int64;
+    fIsRunning: Boolean;
+    method GetElapsed(aMilliseconds: Boolean): Int64;
   protected
   public
     constructor;
     method Start;
     method Stop;
     method Reset;
-    property ElapsedTicks: Int64 read getElapsed( false );
-    property ElapsedMilliseconds: Int64 read getElapsed( true );
+    property ElapsedTicks: Int64 read GetElapsed(false);
+    property ElapsedMilliseconds: Int64 read GetElapsed(true);
     property IsRunning: Boolean read fIsRunning;
   end;
 
@@ -46,15 +46,15 @@ begin
   fIsRunning := false;
 end;
 
-method StopWatch.getElapsed(Milliseconds : Boolean): Int64;
+method StopWatch.GetElapsed(aMilliseconds: Boolean): Int64;
 begin
   if fIsRunning then
     result := DateTime.UtcNow.Ticks - fStartTicks
   else
     result := fEndTicks - fStartTicks;
 
-  if Milliseconds and (result > 0) then
-    result := result / 1000;
+  if aMilliseconds and (result > 0) then
+    result := result / 10 000;
 end;
 
 end.
