9692a9ddbfcb33bda8fd57d80964d65b5b2f33c3
Carlo Kok <ck@remobjects.com>
2021-04-22 21:10:44 +0200

    Implement equals/gethashcode.

--- Source/Memory.cs
+++ Source/Memory.cs
@@ -1,30 +1,30 @@
 ﻿namespace RemObjects.Elements.System
 {
 	public class MemoryException: Exception { }
-	
-    public interface IMemory
-    {
-        object GetValue();
-        void SetValue(object aValue);
-    }
 
-    public __extension class MemoryExtension<T>: Memory<T> {
-        public MemoryExtension(T value) 
-        {
-            var v = value;
-            return &v;
-        }
-    }
-    
+	public interface IMemory
+	{
+		object GetValue();
+		void SetValue(object aValue);
+	}
+
+	public __extension class MemoryExtension<T>: Memory<T> {
+		public MemoryExtension(T value)
+		{
+			var v = value;
+			return &v;
+		}
+	}
+
 	public struct Memory<T>: IMemory {
 		private Object inst;
 		private IntPtr offset;
-		
-        private static void CheckMemorySpace(Memory<T> a, Memory<T> b)
-        {
-            if (a.inst != b.inst) throw new MemoryException("Can not compare items in different memory space");
-        }
-		
+
+		private static void CheckMemorySpace(Memory<T> a, Memory<T> b)
+		{
+			if (a.inst != b.inst) throw new MemoryException("Can not compare items in different memory space");
+		}
+
 		public Memory(Object inst, IntPtr offset)
 		{
 			this.inst = inst;
@@ -39,69 +39,82 @@
 			ref T res = (T*)(*((IntPtr*)(&inst)) + offset);
 			return ref res;
 		}
-		
+
 		public T Value {
 		  get {
-		    return Ref();
+			return Ref();
 		  }
 		  set {
-		    var x = ref Ref();
-		    x = value;
+			var x = ref Ref();
+			x = value;
 		  }
 		}
 
-        object GetValue()
-        {
-            return Value;
-        }
+		object GetValue()
+		{
+			return Value;
+		}
 
-        void SetValue(object aValue)
-        {
-            Value = (T)aValue;
-        }
-        
-        public Memory<TN> Cast<TN>() 
-        {
-            return new Memory<TN>(inst, offset);
-        }
+		void SetValue(object aValue)
+		{
+			Value = (T)aValue;
+		}
 
-        public static bool operator null (Memory<T> a)
+		public override int GetHashCode()
+		{
+			return (inst == null ? 0 : Tuple.R3(inst.GetHashCode())) ^ offset.GetHashCode();
+		}
+
+		public override bool Equals(object val)
+		{
+			var realVal = val as Memory<T>;
+			if (realVal == null) return false;
+
+			return (val.inst == inst) && (val.offset == offset);
+		}
+
+		public Memory<TN> Cast<TN>()
+		{
+			return new Memory<TN>(inst, offset);
+		}
+
+		public static bool operator null (Memory<T> a)
 		{
 			return a.offset == 0 && a.inst == null;
 		}
-        
-        public static bool operator == (Memory<T> a, Memory<T> b) 
-        {
-            return a.inst == b.inst && a.offset == b.offset;
-        }
-		
-        public static bool operator >= (Memory<T> a, Memory<T> b)
-        {
-            CheckMemorySpace(a, b);
-            return a.offset >= b.offset;
-        }
 
-        public static bool operator <= (Memory<T> a, Memory<T> b)
-        {
-            CheckMemorySpace(a, b);
-            return a.offset <= b.offset;
-        }
-        
-        public static bool operator < (Memory<T> a, Memory<T> b)
-        {
-            CheckMemorySpace(a, b);
-            return a.offset < b.offset;
-        }
+		public static bool operator == (Memory<T> a, Memory<T> b)
+		{
+			return a.inst == b.inst && a.offset == b.offset;
+		}
 
-        public static bool operator > (Memory<T> a, Memory<T> b)
-        {
-            CheckMemorySpace(a, b);
-            return a.offset > b.offset;
-        }
-		        
-        public static bool operator != (Memory<T> a, Memory<T> b) 
-        {
-            return !(a.inst == b.inst && a.offset == b.offset);
-        }
+		public static bool operator >= (Memory<T> a, Memory<T> b)
+		{
+			CheckMemorySpace(a, b);
+			return a.offset >= b.offset;
+		}
+
+		public static bool operator <= (Memory<T> a, Memory<T> b)
+		{
+			CheckMemorySpace(a, b);
+			return a.offset <= b.offset;
+		}
+
+		public static bool operator < (Memory<T> a, Memory<T> b)
+		{
+			CheckMemorySpace(a, b);
+			return a.offset < b.offset;
+		}
+
+		public static bool operator > (Memory<T> a, Memory<T> b)
+		{
+			CheckMemorySpace(a, b);
+			return a.offset > b.offset;
+		}
+
+		public static bool operator != (Memory<T> a, Memory<T> b)
+		{
+			return !(a.inst == b.inst && a.offset == b.offset);
+		}
 	}
 }
