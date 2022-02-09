c3d38cf2d9133ca1b771cc5afe520dc9529ed059
marc hoffman <mh@remobjects.com>
2022-02-09 09:22:58 -0400

    Tweaks for unconstrained generics

--- Source/List.pas
+++ Source/List.pas
@@ -52,8 +52,8 @@ type
       if not assigned(Item1) or not assigned(Item2) then exit false;
       case modelOf(T) of
         "Island": exit (Item1 as IslandObject).Equals(Item2 as IslandObject);
-        "Cocoa": exit (Item1 as CocoaObject).isEqualTo(Item2 as CocoaObject);
-        "Swift": exit (Item1 as SwiftObject).Equals(Item2 as SwiftObject);
+        "Cocoa": {$IF DARWIN}exit (Item1 as CocoaObject).isEqualTo(Item2 as CocoaObject);{$ENDIF}
+        "Swift": {$IF DARWIN}exit (Item1 as SwiftObject).Equals(Item2 as SwiftObject);{$ENDIF}
         else raise new Exception($"Unexpected objetc model {modelOf(T)}");
       end;
     end;
