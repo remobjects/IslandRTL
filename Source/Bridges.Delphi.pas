000b0da9b29198bcb05cec59fb7959484ab0085d
marc hoffman <mh@remobjects.com>
2022-02-09 09:01:03 -0400

    Delphi Object Model (prep work)

---
+++ Source/Bridges.Delphi.pas
@@ -0,0 +1,12 @@
+﻿namespace Island.Shared;
+
+{$IF DELPPHI}
+
+type
+  DelphiObject = public Delphi.System.TObject;
+  DelphiString = public Delphi.System.NSString;
+  DelphiException = public Delphi.System.NSException;
+
+{$ENDIF DELPPHI}
+
+end.
