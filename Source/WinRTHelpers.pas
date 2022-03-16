c541900f75073f7cc86d9c7d1ec26b8029fb80d7
evgenyk2 <git2@evgenysoft.com>
2022-03-16 12:50:35 +0300

    0: winrt helpers

---
+++ Source/WinRTHelpers.pas
@@ -0,0 +1,36 @@
+﻿namespace RemObjects.Elements.System;
+
+interface
+
+method CreateWinrtInstance(aName: String): rtl.winrt.IInspectable; public;
+method CreateWinrtFactory(aName: String; var aGUID: Guid): rtl.winrt.IInspectable; public;
+
+implementation
+
+const
+  winrtcore = 'api-ms-win-core-winrt-l1-1-0.dll';
+
+[DelayLoadDllImport(winrtcore, 'RoActivateInstance'), CallingConvention(CallingConvention.Stdcall)]
+method RoActivateInstance(activatableClassId: rtl.winrt.HSTRING; instance: ^rtl.winrt.IInspectable): rtl.HRESULT; external;
+
+[DelayLoadDllImport(winrtcore, 'RoGetActivationFactory'), CallingConvention(CallingConvention.Stdcall)]
+method RoGetActivationFactory(activatableClassId: rtl.winrt.HSTRING; iid: ^rtl.GUID; factory: ^rtl.winrt.IInspectable): rtl.HRESULT; external;
+
+method CreateWinrtInstance(aName: String): rtl.winrt.IInspectable;
+begin
+  var lname: rtl.winrt.HSTRING := aName;
+  var lresult := RoActivateInstance(lname, @result);
+  if lresult <> rtl.S_OK then
+    raise new Exception("error at calling RoActivateInstance, code: " + lresult.ToString);
+end;
+
+method CreateWinrtFactory(aName: String; var aGUID: Guid): rtl.winrt.IInspectable;
+begin
+  var lname: rtl.winrt.HSTRING := aName;
+  var lguid: rtl.GUID := aGUID;
+  var lresult := RoGetActivationFactory(lname, @lguid, @result);
+  if lresult <> rtl.S_OK then
+    raise new Exception("error at calling RoGetActivationFactory, code: " + lresult.ToString);
+end;
+
+end.
