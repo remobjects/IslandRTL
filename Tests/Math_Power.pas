048b06a41ed3eb14bfea49e42c43ebf763261bb0
David Millington <vintagedave@gmail.com>
2025-06-18 11:15:41 +0300

    Merged tests from Math_Power (with renamed method names)

--- Tests/Math_Power.pas
+++ Tests/Math_Power.pas
@@ -6,8 +6,7 @@ uses
 type
   Math_Power = public class(Test)
   public
-
-    method calc;
+    method Calc;
     begin
       var s1,s2: Double;
       var sValue:=10.0;
@@ -22,6 +21,28 @@ type
       Assert.AreEqual(s1, s2);
     end;
 
+    method CalcHalf;
+    begin
+      var s1, s2 : Double;
+      var sValue:=1.0;
+      s1 := Math.Pow(sValue,0.5);
+      s2 := Math.Sqrt(sValue);
+      Assert.AreEqual(s1, s2);
+    end;
+
+    method PowInt;
+    begin
+      var sValue := 2.0;
+      var s1: Double;
+      s1 := Math.Pow(sValue, 5);
+      Assert.AreEqual(s1, 32.0);
+      s1 := Math.Pow(sValue, 8);
+      Assert.AreEqual(s1, 256.0);
+      s1 := Math.Pow(sValue, -5);
+      Assert.AreEqual(s1, 1.0/32.0);
+      s1 := Math.Pow(sValue, -8);
+      Assert.AreEqual(s1, 1.0/256.0);
+    end;
   end;
 
 end.
